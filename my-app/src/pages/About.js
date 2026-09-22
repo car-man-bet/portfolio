50caa51cd35cea25bb42e001d4a4e4faa68e3ae9
car-man-bet <carlos_betancur@alumni.brown.edu>
2026-09-21 23:06:51 -0400

    Update About.js

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -77,8 +77,10 @@ function About() {
           </p>
 
           <p>
-            I hope to pursue a legal career in the  
-              <span className="highlight">Emerging Companies, Venture Capital and/or Capital Markets</span>  
+            I hope to pursue a legal career in the{' '}
+  <span className="highlight">
+    Emerging Companies, Venture Capital and/or Capital Markets
+  </span>{' '} 
               space, employing my legal and tech skills to support innovators from diverse backgrounds.
           </p>
         </section>
